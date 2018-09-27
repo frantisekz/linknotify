c3f3263ea8a32857356d771247bfbc7c6dc1c46a
Lukas Brabec <lbrabec@redhat.com>
2018-09-27 15:55:09 +0200

    Cleanup

--- linknotify-server.py
+++ linknotify-server.py
@@ -9,36 +9,14 @@ context = zmq.Context()
 zmq_socket = context.socket(zmq.PUSH)
 zmq_socket.bind("tcp://127.0.0.1:5557")
 
-URLS = deque([])
 
 @app.route("/send", methods=['POST'])
 def send():
-    global URLS
     data = request.get_json()
 
-    URLS.append(data['url'])
     zmq_socket.send_json(data)
 
-    print(URLS)
-
     return "ok"
-
-@app.route("/get")
-def get():
-    global URLS
-
-    if URLS:
-        url = URLS.popleft()
-        print(URLS)
-        return jsonify({
-            'status': True,
-            'url': url
-        })
-    else:
-        print(URLS)
-        return jsonify({
-            'status': False
-        })
 
 
 if __name__ == "__main__":
