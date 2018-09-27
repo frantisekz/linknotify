7cb47dac9cf97da1bd2ba20e87c84cb5eb2bd390
Lukas Brabec <lbrabec@redhat.com>
2018-09-27 17:32:03 +0200

    publish/subscriber pattern

--- linknotify/linknotify_client.py
+++ linknotify/linknotify_client.py
@@ -8,13 +8,16 @@ from . import settings
 Notify.init("linknotify")
 
 context = zmq.Context()
-consumer_receiver = context.socket(zmq.PULL)
+#consumer_receiver = context.socket(zmq.PULL)
+consumer_receiver = context.socket(zmq.SUB)
 consumer_receiver.connect(settings.SERVER)
+consumer_receiver.setsockopt(zmq.SUBSCRIBE, settings.USER.encode('utf-8'))
 
 def main():
     while True:
-        data = consumer_receiver.recv_json()
-        n = Notify.Notification.new("Link", data['url'], "notification-message-im")
+        #data = consumer_receiver.recv_json()
+        [_, url] = consumer_receiver.recv_multipart()
+        n = Notify.Notification.new("Link", url.decode('utf-8'), "notification-message-im")
         n.show()
 
 
--- linknotify/linknotify_server.py
+++ linknotify/linknotify_server.py
@@ -6,15 +6,21 @@ from . import settings
 
 app = Flask(__name__)
 context = zmq.Context()
-zmq_socket = context.socket(zmq.PUSH)
+#zmq_socket = context.socket(zmq.PUSH)
+zmq_socket = context.socket(zmq.PUB)
 zmq_socket.bind(settings.SERVER)
 
 
 @app.route("/send", methods=['POST'])
 def send():
     data = request.get_json()
+    
+    #zmq_socket.send_json(data)
 
-    zmq_socket.send_json(data)
+    if 'url' not in data or 'user' not in data:
+        return "not ok"
+
+    zmq_socket.send_multipart([data['user'].encode('utf-8'), data['url'].encode('utf-8')])
 
     return "ok"
 
